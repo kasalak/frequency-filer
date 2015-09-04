dcb4a37fb26c3be5ec0069ba056ee0a2278d4c51
Karthik <karthik.kasala@gmail.com>
2015-09-04 15:44:04 -0400

    updated

--- word_frequency.py
+++ word_frequency.py
@@ -1,38 +1,28 @@
-
-#represent all text in sample.txt
 import re
 
 #convert text to all lower case
 def word_frequency(text):
     words = {}
-    for unique in text.split():
-        words = re.sub('[^a-z\ \']+', " ", text).lower
-        if unique in words:
-            words[unique] += 1
+    for word in text.split():
+        word = re.sub(r'[^A-Za-z]', ' ', word).lower()
+        if word in words:
+            words[word] += 1
         else:
-            words[unique] = 1
-    return word_frequency(text)
+            words[word] = 1
+    return words
 
-def file_text():
-    file_name = input("Enter filename here. ")
-    file_text = open(file_name, 'r')
-    text = read(file_name)
-    close(file_name)
-    return file_text()
+def top20(words):
+    for k, v in sorted(words.items(), key=lambda c: c[1], reverse=True)[ :20]:
+        print(a, b)
 
+def get_text():
+    given_file = print(input("Enter filename here. "))
+    book = open(given_file)
+    text = given_file.read()
+    return text
 
-#get rid of sentence structure and convert text file to a list
-#text = re.sub('[^a-z\ \']+', " ", text).lower
-#text = list(text.split())
-#print(text)
+def main():
+    top20(word_frequency(get_text()))
 
-#create dictionary
-#print(text)
-
-#sort words into a list with no duplicates
-
-#make a list of how many times words pop up using .count
-
-
-#sort and print top 20 results
-#pass tests
+if __name__ == '__main__':
+    main()
