5068f117384d942846129bbb84b2609f110c7bb5
Karthik <karthik.kasala@gmail.com>
2015-09-04 14:52:13 -0400

    modified word_frequency.py

--- word_frequency.py
+++ word_frequency.py
@@ -3,20 +3,36 @@
 import re
 
 #convert text to all lower case
-file = open('sample.txt', 'r')
-words = file.read().lower()
-file.close()
-#get rid of sentence structure and convert text file to a list
-words = re.sub('[^a-z\ \']+', " ", words)
-text = list(words.split())
+def word_frequency(text):
+    words = {}
+    for unique in text.split():
+        words = re.sub('[^a-z\ \']+', " ", text).lower
+        if unique in words:
+            words[unique] += 1
+        else:
+            words[unique] = 1
+    return word_frequency(text)
 
+def file_text():
+    file_name = input("Enter filename here. ")
+    file_text = open(file_name, 'r')
+    text = read(file_name)
+    close(file_name)
+    return file_text()
+
+
+#get rid of sentence structure and convert text file to a list
+#text = re.sub('[^a-z\ \']+', " ", text).lower
+#text = list(text.split())
+#print(text)
+
+#create dictionary
 #print(text)
 
 #sort words into a list with no duplicates
-unique_words = []
-number = []
-unique_words = sorted(set(text))
-print(unique_words)
+
 #make a list of how many times words pop up using .count
-for number in unique_words:
-    print(text.count(number), number)
+
+
+#sort and print top 20 results
+#pass tests
